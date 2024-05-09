2953f49225a97adce753ec47f55a9ebee354cd41
KubraGavuzoglu <100765765+KubraGavuzoglu@users.noreply.github.com>
2024-05-09 18:45:49 +0300

    crm

---
+++ src/Sm.Crm.Application/Features/Notifications/Queries/NotificationDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Sm.Crm.Application.Features.Notifications.Queries;
+
+public class NotificationDto
+{
+    public Guid UserId { get; set; }
+    public string? Title { get; set; }
+    public string Description { get; set; } = null!;
+    public bool IsRead { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public Guid CreatedBy { get; set; }
+}
